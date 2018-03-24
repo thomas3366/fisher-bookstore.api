c3f7ab92a385e96f5020b65c21abf4fd9282010b
Katy Thomas <thomas.3366@osu.edu>
2018-03-23 20:03:55 -0400

    Lab 6

---
+++ Migrations/20180321225217_myMigration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Fisher.Bookstore.Api.Migrations
+{
+    public partial class myMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Authors");
+
+            migrationBuilder.RenameColumn(
+                name: "NotableWork",
+                table: "Authors",
+                newName: "Bio");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "Bio",
+                table: "Authors",
+                newName: "NotableWork");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Authors",
+                nullable: true);
+        }
+    }
+}
